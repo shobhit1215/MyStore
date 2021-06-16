013fe9fa0dffd1c0a74057eba4e649a52dfb2b6c
Shobhit <imshobhit.sb@gmail.com>
2021-06-16 19:44:06 +0530

    Created Models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product,Category
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['id','name','price','category']
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['id','name']
+
+
+admin.site.register(Product,AdminProduct)
+admin.site.register(Category,AdminCategory)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+#use of pillow
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    description = models.CharField(max_length=200,default='')
+    image = models.ImageField(upload_to='uploads/products/')
+
+    def __str__(self):
+        return self.name
+
