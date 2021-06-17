a63eced78f5f0fae8a78d6b5fc9b10063ef65428
Shobhit <imshobhit.sb@gmail.com>
2021-06-17 12:01:48 +0530

    Created Home Page

--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+    products = Product.gel_all_products()
+    # print(products)
+    
+    return render(request,'index.html',{'products':products})
